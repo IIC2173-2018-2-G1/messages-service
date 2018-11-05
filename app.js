753f140eb7c10c71714726d738296f6ea1c04049
Ignacio Peñafiel Urzúa <iapenafiel@uc.cl>
2018-11-04 21:24:10 -0300

    GET y POST listos

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var app        = express();
 
 //cambiar el string de abajo para conectarse a base de datos de mongo
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+mongoose.connect('mongodb://localhost:27017',{ useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,7 +21,7 @@ router.post('/message', function(req, res) {
     var message = new Message();
 
     message.channel_id = req.body.channel_id;
-    message.response = req.body.response;
+    message.response_to = req.body.response_to;
     message.content = req.body.content;
 
     // save the bear and check for errors
@@ -30,7 +30,7 @@ router.post('/message', function(req, res) {
             res.json({ message: 'Error creating message',
                        Error: err});
 
-        res.json({ message: 'Message created',
+        res.json({ message: message,
                    Error: ""});
     });
 
@@ -39,14 +39,39 @@ router.post('/message', function(req, res) {
 
 
 router.get('/messages', function(req, res) {
+    hashtag = req.query.hashtag;
+    count = req.query.count;
+    start = req.query.start;
 
-    Message.find({channel_id:req.query.channel_id,
-                  hashtag: req.query.hashtag,
-                  count:req.query.count,
-                  start: req.query.count},
+    Message.find({channel_id: req.query.channel_id},
         function (err, messages) {
             if(err) res.json({messages, Error:err});
 
+            // start filter
+            if (start !== undefined){
+                messages = messages.slice(parseInt(start));
+            };
+
+            // count filter
+            if (count !== undefined){
+                messages = messages.slice(0,parseInt(count));
+            };
+
+            //hashtag filter
+            if (hashtag !== undefined){
+                with_hashtag = []
+                for (i = 0; i < messages.length;i++){
+                    list = messages[i].content.split(" ");
+                    if (list.indexOf("#" + hashtag) >= 0){
+                        with_hashtag.push(messages[i])
+                    }
+                };
+                messages = with_hashtag
+            }
+            
+            //filtrar solo si hashtag in message.content +++++ su split y chao
+            //obtener solo el numero de count +++++ su corte a la lista y chao
+            //partir la cuenta desde start +++++ 
             res.json({messages, Error:""});
     });
 
